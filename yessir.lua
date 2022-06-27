1689137f793024c21923d04e7eac2edeced58d9d
FuckAim <100230789+FuckAim@users.noreply.github.com>
2022-06-28 00:30:06 +0200

    Create yessir.lua

---
+++ yessir.lua
@@ -0,0 +1,11 @@
+_G.Thing = true
+_G.Case = "Helmetalized"
+
+for i=1,700 do
+spawn(function()
+    while _G.Thing do 
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(_G.Case, tostring(game:GetService("Players").LocalPlayer.CasesPer.Value))
+        game:GetService("ReplicatedStorage").Remotes.Click:FireServer()
+    end
+end)
+end
